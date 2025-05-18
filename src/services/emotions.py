c6b222e7041372490e792d49ece098c0e46b085a
Shravan Asati <dev.shravan@protonmail.com>
2025-05-19 01:36:27 +0530

    truncate emotions for now

--- src/services/emotions.py
+++ src/services/emotions.py
@@ -8,12 +8,17 @@ class EmotionAnalysisResult:
     score: float
 
 
+# todo fix index error when exceeding model's token limit
+# chunk the given text as per tokens
+# aggregate each of them
+
 class EmotionClassifier:
     def __init__(self) -> None:
         self.classifier = pipeline(
             "text-classification",
             model="j-hartmann/emotion-english-distilroberta-base",
             top_k=3,
+            truncation=True,
         )
 
     def classify(self, text: str) -> list[EmotionAnalysisResult]:
